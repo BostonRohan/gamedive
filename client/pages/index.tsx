fe5992d8bafee086945f47e37705026c028d2597
Boston Rohan <bostonrohan@Bostons-MBP-2.lan>
2022-07-04 16:31:58 -0400

    tailwind config

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => {
       <Head>
         <title>GameDive</title>
       </Head>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 };
